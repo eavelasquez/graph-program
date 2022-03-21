35a43ff09af93154701cbfe8d19825b32dd8a985
Esteban Velasquez <ev@oiga.com>
2022-03-21 11:59:57 -0500

    feat(back): create java application

---
+++ src/graph/program/GraphProgram.java
@@ -0,0 +1,24 @@
+/******************************************************************************
+ *  Compilation:  javac GraphProgram.java
+ *  Execution:    java GraphProgram
+ *  
+ *  A graph, implementing with adjacency linked list and matrix.
+ *
+ ******************************************************************************/
+package graph.program;
+
+/**
+ *  The {@code GraphProgram} class represents a graph program.
+ *
+ *  @author ev
+ */
+public class GraphProgram {
+
+    /**
+     * Unit tests the {@code GraphProgram} data type.
+     *
+     * @param args the command-line arguments
+     */
+    public static void main(String[] args) {
+    }
+}
