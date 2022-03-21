267110cad3503245b7b8f63d2e6dae0779ab5832
Esteban Velasquez <eavc.30@gmail.com>
2022-03-21 13:13:35 -0500

    feat(front): edit graph program

--- src/graph/program/GraphProgram.java
+++ src/graph/program/GraphProgram.java
@@ -8,9 +8,9 @@
 package graph.program;
 
 /**
- *  The {@code GraphProgram} class represents a graph program.
+ * The {@code GraphProgram} class represents a graph program.
  *
- *  @author ev
+ * @author ev
  */
 public class GraphProgram {
 
@@ -20,5 +20,15 @@ public class GraphProgram {
      * @param args the command-line arguments
      */
     public static void main(String[] args) {
+        int vertexCount = 5;
+        int[][] edges = { { 0, 1 }, { 0, 2 }, { 1, 2 }, { 1, 3 }, { 2, 3 }, { 3, 4 } };
+        Graph graph = new GraphAdjacencyMatrix(vertexCount, edges, false);
+
+        System.out.println(graph.toString());
+
+        int[] visited = new int[vertexCount + 1];
+
+        graph.transversalBfs(1);
+        graph.transversalDfs(1, visited);
     }
 }
